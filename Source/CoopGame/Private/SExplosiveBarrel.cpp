0744233c018f5499241b4a4bc836763117874846
MSI\USER <s.hidalgo@xenocreatives.com>
2018-02-04 02:03:25 +0800

    Finiished TUT 81: Creating Explosive Barrel

---
+++ Source/CoopGame/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SExplosiveBarrel.h"
+#include "SHealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+//#include "Components/StaticMeshComponent.h"
+
+
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComp"));
+	HealthComp->OnHealthChanged.AddDynamic(this, &ASExplosiveBarrel::OnHealthChanged);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetSimulatePhysics(true);
+	//Set to physics body to let radial component affect us
+	MeshComp->SetCollisionObjectType(ECC_PhysicsBody);
+	RootComponent = MeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComp->SetupAttachment(MeshComp);
+	RadialForceComp->Radius = 250;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->bAutoActivate = false;
+	RadialForceComp->bIgnoreOwningActor = true;
+
+	ExplosionImpulse = 400;
+
+
+
+}
+
+
+
+void ASExplosiveBarrel::OnHealthChanged(USHealthComponent* OwningHealthComp, float Health, float HealthDelta, const class UDamageType* DamageType, 
+	class AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (bExploded)
+	{
+		// Nothing left to do, already exploded
+		return;
+	}
+
+	if (Health <= 0.0f)
+	{
+		// Exploded
+		bExploded = true;
+
+		//Boost the barrel upwards
+		FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
+		MeshComp->AddImpulse(BoostIntensity, NAME_None, true);
+
+		//Play FX and change self material to black
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+		
+		//Override material on mesh with blackboard version
+		MeshComp->SetMaterial(0, ExplodedMaterial);
+
+		//Blast away nearby physics actor
+		RadialForceComp->FireImpulse();
+
+		
+	}
+}
+
+
